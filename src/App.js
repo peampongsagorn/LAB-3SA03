4003844f9d30e1f8deb9de4a22063bb541933361
Pongsagorn Juntasuwan <pongsagorn0910@gmail.com>
2022-08-10 17:48:24 +0700

    Checkpoint2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 import './App.css';
 const word = "Hello";
 function App() {
   return (
     <div>
-      {
-        <WordCard value="hello" />
-      }
-
+      <WordCard value="hello"/>
     </div>
   );
 
