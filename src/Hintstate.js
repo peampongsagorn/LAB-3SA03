1fe401b0d4f9769c3924aa2d2922d7ac33b8dc9d
Pongsagorn Juntasuwan <pongsagorn0910@gmail.com>
2022-08-18 14:55:40 +0700

    Second commit

--- src/Hintstate.js
+++ src/Hintstate.js
@@ -1,6 +1,13 @@
 import React from "react";
 export default
 class Hint extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+          fhint: "Click here if you Noopppp"
+        }
+      }
     
+      
   
   }
