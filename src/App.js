0023bf21966534ba02d123ae176bb94cfaf1858f
Pongsagorn Juntasuwan <pongsagorn0910@gmail.com>
2022-08-18 14:37:29 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import WordCard from './WordCard';
 import './App.css';
+import Hint from './Hintstate';
 const word = "Hello";
 function App() {
   return (
     <div>
       <WordCard value="hello"/>
+      <Hint></Hint>
     </div>
   );
 
 }
 export default App;
+
